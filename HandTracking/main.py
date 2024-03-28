718f0fe1386e6c5882c5b19503cf4050f5d1ee68
TypeZero <yushuolu7375@gmail.com>
2024-03-27 20:24:38 -0700

    integrate receiving video streams into UDPCommunication.cs and remove CameraController.cs.

--- HandTracking/main.py
+++ HandTracking/main.py
@@ -1,26 +1,51 @@
 import cv2
 import time
 import HandTrackingModule as htm
+from flask import Flask, request, Response
+from flask_socketio import SocketIO
 
+# pTime = 0
+# cTime = 0
+# cap = cv2.VideoCapture(0)
+# detector = htm.handDetector()
+#
+# while True:
+#     success, img = cap.read()
+#     img = detector.findHands(img)
+#     lmList = detector.findPosition(img)
+#     # if len(lmList) != 0:
+#     #     print(lmList[4])
+#     print(detector.detectIndexFingerFourDirections(lmList))
+#
+#     cTime = time.time()
+#     fps = 1 / (cTime - pTime)
+#     pTime = cTime
+#
+#     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+#
+#     cv2.imshow("Image", img)
+#     cv2.waitKey(1)
 
-pTime = 0
-cTime = 0
-cap = cv2.VideoCapture(0)
-detector = htm.handDetector()
+import socket
+import numpy as np
+import cv2
+
+# UDP服务器设置
+UDP_IP = "0.0.0.0"  # 监听所有来自的IP
+UDP_PORT = 8080     # 设定端口号
+
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
+sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img)
-    # if len(lmList) != 0:
-    #     print(lmList[4])
-    print(detector.detectIndexFingerFourDirections(lmList))
+    data, addr = sock.recvfrom(65535)  # 缓冲大小设置为65k
+    nparr = np.frombuffer(data, np.uint8)
+    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # 将接收到的数据转换为OpenCV图像
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
+    if frame is not None:
+        cv2.imshow("Frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # 按'q'退出
+            break
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+cv2.destroyAllWindows()
--- OrchestraConductor/Assets/Scripts/Systems/UDPCommunication.cs
+++ OrchestraConductor/Assets/Scripts/Systems/UDPCommunication.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CameraController : MonoBehaviour
+public class UDPCommunication : MonoBehaviour
 {
-    public RawImage rawImage; // 在Unity编辑器中将RawImage对象分配给此变量
-
+    public RawImage rawImage;
     private WebCamTexture webCamTexture;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("UDPCommunication Run");
+
         // 获取设备上的摄像头
         WebCamDevice[] devices = WebCamTexture.devices;
 
@@ -19,12 +23,14 @@ public class CameraController : MonoBehaviour
         {
             // 选择第一个摄像头（通常是后置摄像头）
             webCamTexture = new WebCamTexture(devices[0].name);
+            // 开始捕捉摄像头图像
+            webCamTexture.Play();
+
+
             // 将摄像头纹理分配给RawImage对象
             rawImage.texture = webCamTexture;
             // 根据需要水平翻转图像
             rawImage.rectTransform.localScale = new Vector3(-1, 1, 1);
-            // 开始捕捉摄像头图像
-            webCamTexture.Play();
         }
         else
         {
@@ -35,6 +41,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
